7155b64038c1c2c1610c9bea5ef5307c74a79507
sindhu <sindhun@moback.com>
2023-03-08 13:23:01 +0530

    first

---
+++ src/Components/NewExpense/NewExpense.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css'
+
+function NewExpense (props)  {
+  const [isEditing , setEditing] = useState(false)
+  const  saveExpenseDataHandler=(enteredExpenseData) => {
+    const  expenseData ={
+      ...enteredExpenseData,
+      id :Math.random.toString()
+
+    };
+    props.onAddExpense(expenseData);
+
+    setEditing(false)
+
+  };
+
+  const startedEditingHandler = () => {
+    setEditing(true)
+  }
+
+
+  const stopEditingHandler = () => {
+    setEditing(false)
+  }
+  return (
+    <div className= "new-expense">
+    {!isEditing && <button onClick ={startedEditingHandler}>Add New Expense</button>}
+    {isEditing && <ExpenseForm onSaveExpenseData = {saveExpenseDataHandler} onCancel={stopEditingHandler}/>}
+      
+    </div>
+  )
+}
+
+export default NewExpense
